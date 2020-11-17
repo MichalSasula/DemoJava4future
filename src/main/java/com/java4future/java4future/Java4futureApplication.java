65bfb6eca5d98e84e419fa461b316910e4870a03
Michal <michalsasula@gmail.com>
2020-11-17 22:07:46 +0100

    first commit

---
+++ src/main/java/com/java4future/java4future/Java4futureApplication.java
@@ -0,0 +1,13 @@
+package com.java4future.java4future;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java4futureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java4futureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java4future/java4future/Java4futureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java4future.java4future;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java4futureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
