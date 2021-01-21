feb3839ea4a4946509f60113145e37c5266a0971
Michal <michalsasula@gmail.com>
2021-01-21 21:28:37 +0100

    change properties, add banner text

--- src/main/java/com/java4future/java4future/Java4futureApplication.java
+++ src/main/java/com/java4future/java4future/Java4futureApplication.java
@@ -8,6 +8,7 @@ public class Java4futureApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Java4futureApplication.class, args);
+		System.out.println("start");
 	}
 
 }
